f4854d29dd19524f364b211444168c889fa694c4
nickychang4321 <nickychang4321@gmail.com>
2025-05-05 10:02:39 +0800

    v1 5274

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir models
-curl -L "https://www.dropbox.com/scl/fi/50vhbhibsm7r8bc364145/rainbow_icm_best.pth?rlkey=lhvqjg08g8qx5u06setuszl70&st=l53hy3v9&dl=0" -o "rainbow_icm_best.pth"
+curl -L "https://www.dropbox.com/scl/fi/cus7uxf2s791mamt85gmz/rainbow_icm_best.pth?rlkey=af0zt4j3eay0z4tsstc96fk29&st=0xrhiz5e&dl=0" -o "rainbow_icm_best.pth"
 mv rainbow_icm_best.pth models/
